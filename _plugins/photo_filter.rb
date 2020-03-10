6969bf27f2dd39a0af6209b3c8d568fa8e4157d7
Alex Baldwin <alexbaldwin@gmail.com>
2020-03-10 07:57:45 -0700

    Initial fork

--- _plugins/photo_filter.rb
+++ _plugins/photo_filter.rb
@@ -1,11 +1,12 @@
 require "exifr"
+require 'exifr/jpeg'
 
 module Jekyll
   module PhotoFilter
     def photo_filter(files)
       photos = files.select {|photo| photo.relative_path.include?("original") }
       photos.sort_by do |photo|
-        EXIFR::JPEG.new(photo.path).date_time
+        EXIFR::JPEG.new(photo.path).date_time.to_s
       end.reverse
     end
   end
